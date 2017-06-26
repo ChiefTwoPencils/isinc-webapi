bb0329a6d868d850edd77c7ac7cb6739b712f114
ChiefTwoPencils <robertwilk916@gmail.com>
2017-06-26 13:56:19 -0700

    Added Lab2 ApiHouses.

---
+++ ApiHouses/Controllers/HouseController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ApiHouses.Models;
+
+namespace ApiHouses.Controllers
+{
+    public class HouseController : ApiController
+    {
+        private static List<House> houses = new List<House>();
+        private static int nextId;
+
+        static HouseController()
+        {
+            houses.Add(new House(101, "Charlotte", 20000m));
+            houses.Add(new House(102, "Boston", 25000m));
+            houses.Add(new House(103, "Chicago", 300000m));
+            houses.Add(new House(104, "Sacramento", 210000m));
+            nextId = 105;
+        }
+
+        public IEnumerable<House> Get()
+        {
+            return houses;
+        }
+
+        public House Get(int id)
+        {
+            House house = houses.First(h => h.Id == id);
+            if (house != null)
+            {
+                return house;
+            }
+            else
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        public HttpResponseMessage Post(House house)
+        {
+            house.Id = nextId++;
+            houses.Add(house);
+            HttpResponseMessage message = Request.CreateResponse<House>(
+                HttpStatusCode.Created, house
+            );
+            string uri = Url.Link("DefaultApi", new { id = house.Id });
+            message.Headers.Location = new Uri(uri);
+            return message;
+        }
+
+        public void Put(int id, House house)
+        {
+            int index = houses.FindIndex(h => h.Id == id);
+            if (index >= 0)
+            {
+                houses[index] = house;
+            }
+            else
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            int index = houses.FindIndex(h => h.Id == id);
+            if (index >= 0)
+            {
+                houses.RemoveAt(index);
+            }
+            else
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+    }
+}
---
+++ ApiHouses/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ApiHouses")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ApiHouses")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8684d829-4b01-441a-a951-e1ff6e74da22")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
