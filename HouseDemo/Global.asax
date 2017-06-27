b06ea14379f4212aaf22065a2d0ca5452a92866a
ChiefTwoPencils <robertwilk916@gmail.com>
2017-06-27 10:57:52 -0700

    Added Ch.  lab.

---
+++ HouseDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HouseDemo.WebApiApplication" Language="C#" %>
---
+++ HouseDemo/Models/House.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HouseDemo.Models
+{
+    public class House
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public decimal Price { get; set; }
+        public House(int id, string city, decimal price)
+        {
+            Id = id;
+            City = city;
+            Price = price;
+        }
+    }
+}
